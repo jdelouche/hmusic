45e463f95e4b7c926b68e3223206fc46757374fd
Jean-Pierre Delouche <jdelouche@gmail.com>
2019-10-27 13:06:37 +0100

    Type Line

--- src/Bach.hs
+++ src/Bach.hs
@@ -5,23 +5,25 @@ import Data.Music
 import System.IO
 import Data.List.Split
 type SplitCmds = [String]
-codec        ::                                Track Ticks -> Midi
-ravel        ::                                               Midi -> IO ()
-benevolo     ::                [String] -> Track Ticks -> [String] -> IO ()
-preisner     ::                [String] -> Track Ticks -> [String] -> IO ()
-barber       ::                      SplitCmds -> [String] -> [String] -> IO ()
-bach         ::                   SplitCmds -> Track Ticks -> [String] -> IO () 
-wagner       ::                   SplitCmds -> Track Ticks -> [String] -> IO () 
-sibellius    ::                   SplitCmds -> Track Ticks -> [String] -> IO () 
-vivaldi      ::                   SplitCmds -> Track Ticks -> [String] -> IO ()
-rachmaninoff ::          SplitCmds ->Track Ticks -> [String] -> String -> IO ()
-gluck        ::         SplitCmds -> Track Ticks -> [String] -> String -> IO ()
-poulenc      ::       SplitCmds -> Track Ticks -> [String] -> [String] -> IO ()
-mozart       ::         SplitCmds -> Track Ticks -> [String] -> String -> IO ()
-debussy      ::         SplitCmds -> Track Ticks -> [String] -> String -> IO () 
-arvopart     ::         SplitCmds -> Track Ticks -> [String] -> String -> IO () 
-beethoven    ::         SplitCmds -> Track Ticks -> [String] -> String -> IO () 
-lully        ::         SplitCmds -> Track Ticks -> [String] -> String -> IO () 
+type Notes = [String]
+type Line = String
+codec        ::                                  Track Ticks -> Midi
+ravel        ::                                                 Midi -> IO ()
+benevolo     ::                  [String] -> Track Ticks -> [String] -> IO ()
+preisner     ::                  [String] -> Track Ticks -> [String] -> IO ()
+barber       ::                    SplitCmds -> [String] -> [String] -> IO ()
+bach         ::                    SplitCmds -> Track Ticks -> Notes -> IO () 
+wagner       ::                 SplitCmds -> Track Ticks -> [String] -> IO () 
+sibellius    ::                 SplitCmds -> Track Ticks -> [String] -> IO () 
+vivaldi      ::                 SplitCmds -> Track Ticks -> [String] -> IO ()
+rachmaninoff ::          SplitCmds ->Track Ticks -> [String] -> Line -> IO ()
+gluck        ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO ()
+poulenc      ::     SplitCmds -> Track Ticks -> [String] -> [String] -> IO ()
+mozart       ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO ()
+debussy      ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO () 
+arvopart     ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO () 
+beethoven    ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO () 
+lully        ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO () 
 codec n               = Midi { fileType = MultiTrack,timeDiv  = TicksPerBeat 24,tracks   = [n] } 
 ravel m               = exportFile "mymusic.mid" m
 charpentier           = ravel . codec
@@ -33,10 +35,10 @@ arvopart l xs ys x    = do let (n,m) = ((xs++(notmi x)),(ys++[x])) in gluck l n 
 barber l cory ys      = do print ("Erasing:"++(last ys)) ; print cory
 vivaldi l xs ys       = do let (cor,cory) = ((init . init) xs, init ys) in poulenc l cor cory ys
 poulenc l cor cory ys = do barber l cory ys ; benevolo l cor cory
+sibellius l xs ys     = let (z:zs)=l in mozart zs xs ys z
+lully l xs ys x       = if (length l /= -1) then sibellius l xs ys else mozart [] xs ys x
+beethoven l xs ys x   = do let l=splitOn "," x in lully l xs ys x
 debussy l xs ys x     = if (x/="x") then arvopart l xs ys x else vivaldi l xs ys
 mozart l xs ys x      = if (x/="") then debussy l xs ys x else bach l xs ys
-lully l xs ys x       = if (length l /= -1) then sibellius l xs ys else mozart [] xs ys x
-sibellius l xs ys     = let (z:zs)=l in mozart zs xs ys z
-beethoven l xs ys x   = do let l=splitOn "," x in lully l xs ys x
 wagner l xs ys        = do x<-getLine; beethoven l xs ys x
 bach l xs ys          = if (length l == 0 ) then wagner l xs ys else let (z:zs) = l in mozart zs xs ys z

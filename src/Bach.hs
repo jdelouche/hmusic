a13e76ca89244101891897981bf705da700fffb9
Jean-Pierre Delouche <jdelouche@gmail.com>
2019-10-27 18:01:48 +0100

    Line

--- src/Bach.hs
+++ src/Bach.hs
@@ -5,20 +5,22 @@ import Data.Music
 import System.IO
 import Data.List.Split
 type SplitCmds = [String]
-type Notes = [String]
-type Line = String
+type Notes     = [String]
+type Line      = String
+type Note      = String
+data Data      = Line | Note
 ravel        ::                                                 Midi -> IO ()
-codec        ::                                  Track Ticks -> Midi
-benevolo     ::                  [String] -> Track Ticks -> [String] -> IO ()
-preisner     ::                  [String] -> Track Ticks -> [String] -> IO ()
-barber       ::                    SplitCmds -> [String] -> [String] -> IO ()
+codec        ::                                 Track Ticks  -> Midi
+benevolo     ::                    SplitCmds -> Track Ticks -> Notes -> IO ()
+preisner     ::                    SplitCmds -> Track Ticks -> Notes -> IO ()
+barber       ::                          SplitCmds -> Notes -> Notes -> IO ()
+poulenc      ::           SplitCmds -> Track Ticks -> Notes -> Notes -> IO ()
 bach         ::                    SplitCmds -> Track Ticks -> Notes -> IO () 
-wagner       ::                 SplitCmds -> Track Ticks -> [String] -> IO () 
-sibellius    ::                 SplitCmds -> Track Ticks -> [String] -> IO () 
-vivaldi      ::                 SplitCmds -> Track Ticks -> [String] -> IO ()
-rachmaninoff ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO ()
-gluck        ::         SplitCmds -> Track Ticks -> [String] -> Line -> IO ()
-poulenc      ::     SplitCmds -> Track Ticks -> [String] -> [String] -> IO ()
+wagner       ::                    SplitCmds -> Track Ticks -> Notes -> IO () 
+sibellius    ::                    SplitCmds -> Track Ticks -> Notes -> IO () 
+vivaldi      ::                    SplitCmds -> Track Ticks -> Notes -> IO ()
+rachmaninoff ::            SplitCmds -> Track Ticks -> Notes -> Line -> IO ()
+gluck        ::            SplitCmds -> Track Ticks -> Notes -> Line -> IO ()
 mozart       ::            SplitCmds -> Track Ticks -> Notes -> Line -> IO ()
 debussy      ::            SplitCmds -> Track Ticks -> Notes -> Line -> IO () 
 arvopart     ::            SplitCmds -> Track Ticks -> Notes -> Line -> IO () 
@@ -36,7 +38,7 @@ barber l cory ys      = do print ("Erasing:"++(last ys)) ; print cory
 vivaldi l xs ys       = do let (cor,cory) = ((init . init) xs, init ys) in poulenc l cor cory ys
 poulenc l cor cory ys = do barber l cory ys ; benevolo l cor cory
 sibellius l xs ys     = let (z:zs)=l in mozart zs xs ys z
-lully     l xs ys x   = if (length l /= 0) then sibellius l xs ys else mozart [] xs ys x
+lully     l xs ys x   = if (length l == 0) then mozart [] xs ys x else sibellius l xs ys
 beethoven l xs ys x   = do let l=splitOn "," x in lully l xs ys x
 debussy   l xs ys x   = if (x/="x") then arvopart l xs ys x else vivaldi l xs ys
 mozart    l xs ys x   = if (x/="") then debussy l xs ys x else bach l xs ys

eb2bb8541abef26e47e2c8015fd24c253a32ec7c
Jean-Pierre Delouche <jdelouche@gmail.com>
2019-10-28 22:12:40 +0100

    Params

--- src/Bach.hs
+++ src/Bach.hs
@@ -10,10 +10,10 @@ type Line      = String
 type Note      = String
 type Token     = String
 data Data      = Line | Note
+data Params    = Params { tokens :: Tokens, tracks :: Track Ticks, notes :: Notes } deriving (Eq,Show)
 ravel        ::                                                 Midi -> IO ()
 codec        ::                                 Track Ticks  -> Midi
 preisner     ::                    Tokens -> Track Ticks -> Notes -> IO ()
-rachmaninoff ::            Tokens -> Track Ticks -> Notes -> Line -> IO ()
 gluck        ::            Tokens -> Track Ticks -> Notes -> Line -> IO ()
 benevolo     ::                    Tokens -> Track Ticks -> Notes -> IO ()
 barber       ::                          Tokens -> Notes -> Notes -> IO ()
@@ -26,14 +26,14 @@ vivaldi      ::                    Tokens -> Track Ticks -> Notes -> IO ()
 debussy      ::            Tokens -> Track Ticks -> Notes -> Line -> IO () 
 mozart       ::            Tokens -> Track Ticks -> Notes -> Line -> IO ()
 wagner       ::                    Tokens -> Track Ticks -> Notes -> IO () 
-bach         ::                    Tokens -> Track Ticks -> Notes -> IO () 
-codec n               = Midi { fileType = MultiTrack,timeDiv  = TicksPerBeat 24,tracks   = [n] } 
+--bach         ::                    Tokens -> Track Ticks -> Notes -> IO () 
+bach         ::                    Params -> IO () 
+codec n               = Midi { fileType = MultiTrack,timeDiv  = TicksPerBeat 24,Codec.Midi.tracks   = [n] } 
 ravel m               = exportFile "mymusic.mid" m
 charpentier           = ravel . codec
-benevolo l xs ys      = do charpentier xs ; bach l xs ys
+benevolo l xs ys      = do charpentier xs ; bach (Params l xs ys)
 preisner l x m        = do let (z:zs) = m in print $ z++","++(foldr(\a x -> x++a++",") "" (reverse zs))
-gluck l n m x         = if (x=="q") then do preisner l n m else do rachmaninoff l n m x
-rachmaninoff l n m x  = benevolo l n m
+gluck l n m x         = if (x=="q") then preisner l n m else benevolo l n m
 arvopart l xs ys x    = do let (n,m) = ((xs++(notmi x)),(ys++[x])) in gluck l n m x
 barber l cory ys      = do print ("Erasing:"++(last ys)) ; print cory
 vivaldi l xs ys       = do let (cor,cory) = ((init . init) xs, init ys) in poulenc l cor cory ys
@@ -42,6 +42,6 @@ sibellius l xs ys     = let (z:zs)=l in mozart zs xs ys z
 lully     l xs ys x   = if (length l == 0) then mozart [] xs ys x else sibellius l xs ys
 beethoven l xs ys x   = do let l=splitOn "," x in lully l xs ys x
 debussy   l xs ys x   = if (x/="x") then arvopart l xs ys x else vivaldi l xs ys
-mozart    l xs ys x   = if (x/="") then debussy l xs ys x else bach l xs ys
+mozart    l xs ys x   = if (x/="") then debussy l xs ys x else bach (Params l xs ys)
 wagner    l xs ys     = do x<-getLine; beethoven l xs ys x
-bach      l xs ys     = if (length l == 0 ) then wagner l xs ys else let (z:zs) = l in mozart zs xs ys z
+bach (Params l xs ys) = if (length l == 0 ) then wagner l xs ys else let (z:zs) = l in mozart zs xs ys z
--- src/Main.hs
+++ src/Main.hs
@@ -2,4 +2,4 @@ module Main where
 import Bach
 import Codec.Midi
 main :: IO ()
-main = bach [] [(0,Text "Start"),(0,Text "Start")] ["start"]
+main = bach (Params [] [(0,Text "Start"),(0,Text "Start")] ["start"])

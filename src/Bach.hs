0495ddb6e2fbde44be4007d6262a62753f7764b3
Jean-Pierre Delouche <jdelouche@gmail.com>
2019-10-29 23:08:24 +0100

    gluck

--- src/Bach.hs
+++ src/Bach.hs
@@ -11,22 +11,22 @@ type Note      = String
 type Token     = String
 data Data      = Line | Note
 data Ps    = Ps { tokens :: Tokens, tracks :: Track Ticks, notes :: Notes } deriving (Eq,Show)
-ravel        ::                                              Midi -> IO ()
-codec        ::                              Track Ticks  -> Midi
-preisner     ::                                                Ps -> IO ()
-gluck        ::                                        Ps -> Line -> IO ()
-benevolo     ::                                                Ps -> IO ()
-barber       ::                          Tokens -> Notes -> Notes -> IO ()
-poulenc      ::                                       Ps -> Notes -> IO ()
-sibellius    ::                                                Ps -> IO () 
-lully        ::                                        Ps -> Line -> IO () 
-beethoven    ::                                        Ps -> Line -> IO () 
-arvopart     ::                                        Ps -> Line -> IO () 
-vivaldi      ::                                                Ps -> IO ()
-debussy      ::                                        Ps -> Line -> IO () 
-mozart       ::                                        Ps -> Line -> IO ()
-wagner       ::                                                Ps -> IO () 
-bach         ::                                                Ps -> IO () 
+ravel        ::                             Midi -> IO ()
+codec        ::             Track Ticks  -> Midi
+preisner     ::                               Ps -> IO ()
+gluck        ::                       Ps -> Line -> IO ()
+benevolo     ::                               Ps -> IO ()
+barber       ::         Tokens -> Notes -> Notes -> IO ()
+poulenc      ::                      Ps -> Notes -> IO ()
+sibellius    ::                               Ps -> IO () 
+lully        ::                       Ps -> Line -> IO () 
+beethoven    ::                       Ps -> Line -> IO () 
+arvopart     ::                       Ps -> Line -> IO () 
+vivaldi      ::                               Ps -> IO ()
+debussy      ::                       Ps -> Line -> IO () 
+mozart       ::                       Ps -> Line -> IO ()
+wagner       ::                               Ps -> IO () 
+bach         ::                               Ps -> IO () 
 codec n = Midi { fileType = MultiTrack, timeDiv  = TicksPerBeat 24, Codec.Midi.tracks   = [n] } 
 ravel m                        = exportFile "mymusic.mid" m
 charpentier                    = ravel . codec
@@ -37,7 +37,7 @@ gluck p  x                     = benevolo p
 arvopart (Ps tko tr ns) x      = let (n,m) = ((tr++(notmi x)),(ns++[x])) in gluck (Ps tko n m) x
 barber tko xtr ns              = do print ("Erasing:"++(last ns)) ; print xtr
 vivaldi (Ps l tr ns)           = let (xtr,xn) = ((init . init) tr, init ns) in poulenc (Ps l xtr xn) ns
-poulenc p@(Ps tko _ xns) ns   = do barber tko xns ns ; benevolo p
+poulenc p@(Ps tko _ xns) ns    = do barber tko xns ns ; benevolo p
 sibellius p@(Ps (t:ts)  tr ns) = mozart (Ps ts tr ns) t
 lully     p@(Ps [] tr ns) x    = mozart p x
 lully     p x                  = sibellius p

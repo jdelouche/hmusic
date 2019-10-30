50ae8ead6f1483f2ea3b2dc9208e9dee9cd3f47a
Jean-Pierre Delouche <jdelouche@gmail.com>
2019-10-30 19:53:00 +0100

    gluck

--- src/Bach.hs
+++ src/Bach.hs
@@ -37,15 +37,15 @@ gluck p  x                       = benevolo p
 arvopart (Glob tko tr ns) x      = let (n,m) = ((tr++(notmi x)),(ns++[x])) in gluck (Glob tko n m) x
 barber tko xtr ns                = do print ("Erasing:"++(last ns)) ; print xtr
 vivaldi (Glob l tr ns)           = let (xtr,xn) = ((init . init) tr, init ns) in poulenc (Glob l xtr xn) ns
-poulenc p@(Glob tko _ xns) ns    = do barber tko xns ns ; benevolo p
+poulenc   p@(Glob tko _ xns) ns  = do barber tko xns ns ; benevolo p
 sibellius p@(Glob (t:ts)  tr ns) = mozart (Glob ts tr ns) t
-lully     p@(Glob [] tr ns) x    = mozart p x
+lully     p@(Glob [] _ _) x      = mozart p x
 lully     p x                    = sibellius p
-beethoven p@(Glob tko tr ns) x   = let tko'=splitOn "," x in lully (Glob tko' tr ns) x
+beethoven p@(Glob _ tr ns)  x    = let tko'=splitOn "," x in lully (Glob tko' tr ns) x
 debussy   p "x"                  = vivaldi p
 debussy   p x                    = arvopart p x
-mozart p@(Glob tko tr ns) ""     = bach p
-mozart p@(Glob tko tr ns) x      = debussy p x
-wagner p@(Glob tko tr ns)        = do x<-getLine; beethoven p x
-bach p@(Glob []  tr ns)          = wagner p
-bach p@(Glob (t:ts) tr ns)       = mozart (Glob ts tr ns) t
+mozart    p ""                   = bach p
+mozart    p x                    = debussy p x
+wagner    p                      = do x<-getLine; beethoven p x
+bach      p@(Glob []  tr ns)     = wagner p
+bach      p@(Glob (t:ts) tr ns)  = mozart (Glob ts tr ns) t

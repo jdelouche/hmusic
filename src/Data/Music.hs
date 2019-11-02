d5974a3d98d4a588b8fffe3035fcb7927eae11f1
Jean-Pierre Delouche <jdelouche@gmail.com>
2019-11-02 18:01:21 +0100

    foldr

--- src/Data/Music.hs
+++ src/Data/Music.hs
@@ -1,23 +1,23 @@
 module Data.Music where
 import Prelude
 import Codec.Midi
-notmi::Num a => String -> [(a, Message)]
-notmi "pause" = [(0,  NoteOn 0 60 0),(24, NoteOff 0 60 0)]
-notmi ('#':s) = [(0,Text s)]
-notmi "end"   = [(0,TrackEnd)]
-notmi "fin"   = [(0,TrackEnd)]
-notmi (a:l:o:d:[]) = case d of
-                          '-' -> fmap blanche (notmi (a:l:o:[]))
-                          '_' -> fmap (blanche . blanche) (notmi (a:l:o:[]))
-                          _   -> notmi (a:l:o:[])
-notmi (x:y:z:[]) = case z of
-                          '-' -> fmap blanche (notmi (x:y:[]))
-                          '_' -> fmap (blanche . blanche) (notmi (x:y:[]))
+notmi::Num a => Int -> String -> [(a, Message)]
+notmi c "pause" = [(0,  NoteOn c 60 0),(24, NoteOff c 60 0)]
+notmi c ('#':s) = [(0,Text s)]
+notmi c "end"   = [(0,TrackEnd)]
+notmi c "fin"   = [(0,TrackEnd)]
+notmi c (a:l:o:d:[]) = case d of
+                          '-' -> fmap blanche (notmi c (a:l:o:[]))
+                          '_' -> fmap (blanche . blanche) (notmi c (a:l:o:[]))
+                          _   -> notmi c (a:l:o:[])
+notmi c (x:y:z:[]) = case z of
+                          '-' -> fmap blanche (notmi c (x:y:[]))
+                          '_' -> fmap (blanche . blanche) (notmi c (x:y:[]))
                           _   -> case x of
-                                      'l' -> fmap low (notmi (y:z:[]))
-                                      'h' -> fmap hight (notmi (y:z:[]))
-                                      _   -> notmi (y:z:[])
-notmi (l:o:[]) = let m = case o of
+                                      'l' -> fmap low (notmi c (y:z:[]))
+                                      'h' -> fmap hight (notmi c (y:z:[]))
+                                      _   -> notmi c (y:z:[])
+notmi c (l:o:[]) = let m = case o of
                              '0' -> 48
                              '1' -> 60
                              '2' -> 72
@@ -25,16 +25,16 @@ notmi (l:o:[]) = let m = case o of
                              '4' -> 96
                              _   -> 60
                              in case l of
-                                     'c' -> [(0,  NoteOn 0 m      80),(24, NoteOff 0 m      0)]
-                                     'd' -> [(0,  NoteOn 0 (m+2)  80),(24, NoteOff 0 (m+2)  0)]
-                                     'e' -> [(0,  NoteOn 0 (m+4)  80),(24, NoteOff 0 (m+4)  0)]
-                                     'f' -> [(0,  NoteOn 0 (m+5)  80),(24, NoteOff 0 (m+5)  0)]
-                                     'g' -> [(0,  NoteOn 0 (m+7)  80),(24, NoteOff 0 (m+7)  0)]
-                                     'a' -> [(0,  NoteOn 0 (m+9)  80),(24, NoteOff 0 (m+9)  0)]
-                                     'b' -> [(0,  NoteOn 0 (m+11) 80),(24, NoteOff 0 (m+11) 0)]
-                                     _   -> [(0,  NoteOn 0 m      80),(24, NoteOff 0 m      0)]
+                                     'c' -> [(0,  NoteOn c m      80),(24, NoteOff c m      0)]
+                                     'd' -> [(0,  NoteOn c (m+2)  80),(24, NoteOff c (m+2)  0)]
+                                     'e' -> [(0,  NoteOn c (m+4)  80),(24, NoteOff c (m+4)  0)]
+                                     'f' -> [(0,  NoteOn c (m+5)  80),(24, NoteOff c (m+5)  0)]
+                                     'g' -> [(0,  NoteOn c (m+7)  80),(24, NoteOff c (m+7)  0)]
+                                     'a' -> [(0,  NoteOn c (m+9)  80),(24, NoteOff c (m+9)  0)]
+                                     'b' -> [(0,  NoteOn c (m+11) 80),(24, NoteOff c (m+11) 0)]
+                                     _   -> [(0,  NoteOn c m      80),(24, NoteOff c m      0)]
 
-notmi s = [(0,Text s)]
+notmi c s = [(0,Text s)]
 low::(a, Message) -> (a, Message)
 low      (d,NoteOn  x m v) = (d,NoteOn    x (m-1) v)
 low      (d,NoteOff x m v) = (d,NoteOff   x (m-1) v)
--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,21 @@
 module Main where
 import Prelude
-import Bach
 import Codec.Midi
 import Data.List.Split
 import System.IO
--- bach (Glob 1 [] [(0,Text "Start"),(0,Text "Start")] ["start"])
---
-check ts ('q':xs) = do
-                       let (tr1,tr2,tr3) = (foldr (\t trs -> case t of
-                                                                  (a:[])     -> a:trs
-                                                                  (a:b:[])   -> a:trs
-                                                                  (a:b:c:[]) -> a:trs
-                                                                  _          -> "_":trs) [] ts,
-                                            foldr (\t trs -> case t of 
-                                                                  (a:b:[])   -> b:trs
-                                                                  (a:b:c:[]) -> b:trs
-                                                                  _          -> "pause":trs) [] ts,
-                                            foldr (\t trs -> case t of 
-                                                                  (a:b:[])   -> "pause":trs
-                                                                  (a:b:c:[]) -> c:trs
-                                                                  _          -> "pause":trs) [] ts)
-                           in bach (Glob 1 tr1 [(0,Text "Start"),(0,Text "Start")] ["start"])
-check y  _ = looping y
-looping y  = do x <- getLine
-                check (y ++ [splitOn " " x]) x
+data GMain = Gmain { tkos::[[String]] }
+tup f1 f2 f3 = foldr (\t trs -> case t of
+                            (a:[])     -> f1 a trs
+                            (a:b:[])   -> f2 a b trs
+                            (a:b:c:[]) -> f3 a b c trs
+                            _          -> "?":trs) []
+one = tup (\a trs -> a:trs)       (\a _ trs -> a:trs)       (\a _ _ trs -> a:trs)
+two = tup (\_ trs -> "pause":trs) (\_ b trs -> b:trs)       (\_ b _ trs -> b:trs)
+thr = tup (\_ trs -> "pause":trs) (\_ _ trs -> "pause":trs) (\_ _ c trs -> c:trs)
+looping (Gmain y) = do (allx@(x:xs)) <- getLine
+                       case x of
+                              'q' -> do let trs@(tr1,tr2,tr3) = (one y,two y,thr y)
+                                        print trs
+                              _   -> looping (Gmain (y++[splitOn " " allx]))
 main :: IO ()
-main = looping []
+main = looping (Gmain [])

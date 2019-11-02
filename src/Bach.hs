c24d9700a2419cd11b4466974dc8ce89b5ba218b
Jean-Pierre Delouche <jdelouche@gmail.com>
2019-11-02 15:58:30 +0100

    init

--- src/Bach.hs
+++ src/Bach.hs
@@ -9,7 +9,7 @@ type Notes     = [Note]
 type Line      = String
 type Note      = String
 type Token     = String
-data Glob    = Glob { tokens :: Tokens, tracks :: Track Ticks, notes :: Notes } deriving (Eq,Show)
+data Glob    = Glob { channel::Int, tokens :: Tokens, tracks :: Track Ticks, notes :: Notes } deriving (Eq,Show)
 ravel        ::                               Midi -> IO ()
 codec        ::               Track Ticks  -> Midi
 preisner     ::                               Glob -> IO ()
@@ -29,23 +29,23 @@ bach         ::                               Glob -> IO ()
 codec n = Midi { fileType = MultiTrack, timeDiv  = TicksPerBeat 24, Codec.Midi.tracks   = [n] } 
 ravel m                          = exportFile "mymusic.mid" m
 charpentier                      = ravel . codec
-benevolo  p@(Glob _ tr _)        = do charpentier tr ; bach p
-preisner    (Glob _ _ (z:zs))    = let m=britten z zs in do print m; writeFile "mymusic.txt" m
+benevolo  p@(Glob c _ tr _)        = do charpentier tr ; bach p
+preisner    (Glob c _ _ (z:zs))    = let m=britten z zs in do print m; writeFile "mymusic.txt" m
 britten                z zs      = z++","++(foldr(\a x -> x++a++",") "" (reverse zs))
 gluck     p "q"                  = preisner p 
 gluck     p  x                   = benevolo p 
-arvopart    (Glob tko tr ns) x   = let (n,m) = ((tr++(notmi x)),(ns++[x])) in gluck (Glob tko n m) x
+arvopart    (Glob c tko tr ns) x   = let (n,m) = ((tr++(notmi c x)),(ns++[x])) in gluck (Glob c tko n m) x
 barber tko xtr ns                = do print ("Erasing:"++(last ns)) ; print xtr
-vivaldi     (Glob l tr ns)       = let (xtr,xn) = ((init . init) tr, init ns) in poulenc (Glob l xtr xn) ns
-poulenc   p@(Glob tko _ xns) ns  = do barber tko xns ns ; benevolo p
-sibellius p@(Glob (t:ts) tr ns)  = mozart (Glob ts tr ns) t
-lully     p@(Glob [] _ _) x      = mozart p x
+vivaldi     (Glob c l tr ns)       = let (xtr,xn) = ((init . init) tr, init ns) in poulenc (Glob c l xtr xn) ns
+poulenc   p@(Glob c tko _ xns) ns  = do barber tko xns ns ; benevolo p
+sibellius p@(Glob c (t:ts) tr ns)  = mozart (Glob c ts tr ns) t
+lully     p@(Glob c [] _ _) x      = mozart p x
 lully     p x                    = sibellius p
-beethoven p@(Glob _ tr ns)  x    = let tko'=splitOn "," x in lully (Glob tko' tr ns) x
+beethoven p@(Glob c _ tr ns)  x    = let tko'=splitOn "," x in lully (Glob c tko' tr ns) x
 debussy   p "x"                  = vivaldi p
 debussy   p x                    = arvopart p x
 mozart    p ""                   = bach p
 mozart    p x                    = debussy p x
 wagner    p                      = do x<-getLine; beethoven p x
-bach      p@(Glob []  tr ns)     = wagner p
-bach      p@(Glob (t:ts) tr ns)  = mozart (Glob ts tr ns) t
+bach      p@(Glob c []  tr ns)     = wagner p
+bach      p@(Glob c (t:ts) tr ns)  = mozart (Glob c ts tr ns) t
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,28 @@
 module Main where
+import Prelude
 import Bach
 import Codec.Midi
+import Data.List.Split
+import System.IO
+-- bach (Glob 1 [] [(0,Text "Start"),(0,Text "Start")] ["start"])
+--
+check ts ('q':xs) = do
+                       let (tr1,tr2,tr3) = (foldr (\t trs -> case t of
+                                                                  (a:[])     -> a:trs
+                                                                  (a:b:[])   -> a:trs
+                                                                  (a:b:c:[]) -> a:trs
+                                                                  _          -> "_":trs) [] ts,
+                                            foldr (\t trs -> case t of 
+                                                                  (a:b:[])   -> b:trs
+                                                                  (a:b:c:[]) -> b:trs
+                                                                  _          -> "pause":trs) [] ts,
+                                            foldr (\t trs -> case t of 
+                                                                  (a:b:[])   -> "pause":trs
+                                                                  (a:b:c:[]) -> c:trs
+                                                                  _          -> "pause":trs) [] ts)
+                           in bach (Glob 1 tr1 [(0,Text "Start"),(0,Text "Start")] ["start"])
+check y  _ = looping y
+looping y  = do x <- getLine
+                check (y ++ [splitOn " " x]) x
 main :: IO ()
-main = bach (Glob [] [(0,Text "Start"),(0,Text "Start")] ["start"])
+main = looping []

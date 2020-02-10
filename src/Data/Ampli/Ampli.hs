b4d15dd043a0047bbe1fa21f7ff32b9ba2d4b822
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-10 21:31:55 +0100

    indent

--- src/Data/Ampli/Ampli.hs
+++ src/Data/Ampli/Ampli.hs
@@ -3,23 +3,23 @@ module Data.Ampli.Ampli where
 import Data.Ampli.Hylo
 data StreamF  e a = StreamF e a deriving (Functor,Show)
 data ChannelF e a = NilF | ChannelF e a deriving (Functor,Show)
-type Carrier    = Either Receiver Sender
-type Transfer   = Maybe Int
-type ConnectorF = ChannelF Transfer
-type InterfaceF = ConnectorF Carrier
+type Carrier      = Either Receiver Sender
+type Transfer     = Maybe Int
+type ConnectorF   = ChannelF Transfer
+type InterfaceF   = ConnectorF Carrier
 output  ::            Fix (ConnectorF) -> Carrier
 input   :: Carrier -> Fix (ConnectorF)
 output  = cata send
 input   = ana receive
-ampli = (output . input)
+ampli   = (output . input)
 type Receiver = [Char]
 type Sender   = [Int]
 send::               InterfaceF -> Carrier
 receive:: Carrier -> InterfaceF
 send   (ChannelF Nothing   (Right p))  = (Right p)
 send   (ChannelF (Just i)  (Right p))  = (Right (i:p))
-send   (NilF)                         = (Right [])
+send   (NilF)                          = (Right [])
 receive (Left [])           = NilF
 receive (Left ('e':'1':ns)) = ChannelF (Just 48) (Left ns)
 receive (Left ('e':'2':ns)) = ChannelF (Just 49) (Left ns)
-receive (Left (p : ns))     = ChannelF (Nothing) (Left ns)
+receive (Left (p      :ns)) = ChannelF (Nothing) (Left ns)

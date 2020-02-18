4cf2b73b95efa2e753a907ac83261bfa0573eb80
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-18 21:42:33 +0100

    poing

--- src/Main.hs
+++ src/Main.hs
@@ -4,16 +4,16 @@ import Data.Ampli.Ampli as Midi
 import Data.Ampli.Text.Sep as Sep
 import Codec.Midi
 import Data.Typeable
-midi a = sequence $ fmap (\(c,s) -> Midi.ampli (Left (c,s))) a
+midi a = sequence $ fmap (\(c,l,s) -> Midi.ampli (Left (c,s))) a
 unRight (Right a) = a
 (_R_)             = unRight
-parser x          =  _R_ $ midi $ _R_ $ Sep.ampli $ Left(False,1,x)
+parser x          =  _R_ $ midi $ _R_ $ Sep.ampli $ Left(False,1,1,x)
 codecmulti n      = Midi { fileType = MultiTrack, timeDiv  = TicksPerBeat 24, Codec.Midi.tracks   = n }
 loop d            = do 
-                       print d
-                       print $ Sep.ampli $ Left(False,1,d)
-                       --print $ parser d
-                       --exportFile "mymusic.mid" $ codecmulti $ parser d
+                       print $ d
+                       print $ Sep.ampli $ Left(False,1,1,d)
+                       print $ parser d
+                       exportFile "mymusic.mid" $ codecmulti $ parser d
                        x <- getChar
                        case x of
                             'q'    -> putStrLn ""

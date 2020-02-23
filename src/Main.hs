0f245fa19f69aa2b4043d7ff114a5d5849b653a1
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-23 10:00:45 +0100

    test

--- src/Main.hs
+++ src/Main.hs
@@ -5,20 +5,21 @@ import Data.Amp.Text.Sep        as Sep
 import Data.Amp.Text.Table      as Table
 import Codec.Midi
 import Data.Typeable
-tmidi::[[(Int,[Char])]] -> [[(Ticks, Message)]]
-tmidi = fmap (foldr (\x a -> (Midi.midi x)++a) [])
+midchannels::[[(Int,[Char])]] -> [[(Ticks, Message)]]
+midchannels = fmap (foldr (\x a -> (Midi.midi x)++a) [])
 parser :: String -> [[(Int,[Char])]]
-parser x          = Table.table $ Sep.sep x
+parser x = Table.table $ Sep.sep $ x
 tomidi :: String -> [[(Ticks, Message)]]
-tomidi x          = Main.tmidi $ parser x
-codecmulti n      = Midi { fileType = MultiTrack, timeDiv  = TicksPerBeat 24, Codec.Midi.tracks   = n }
-loop d            = do 
-                       exportFile "mymusic.mid" $ codecmulti $ tomidi d
-                       x <- getChar
-                       case x of
-                            'q'    -> putStrLn ""
-                            '\DEL' -> case d of
-                                           [] -> loop []
-                                           _  -> loop $ init d
-                            _      -> loop $ d++[x]
+tomidi x = midchannels $ parser $ x
+codecmulti n = Midi { fileType = MultiTrack, timeDiv  = TicksPerBeat 24, Codec.Midi.tracks   = n }
+loop d       = do 
+                  exportFile "mymusic.mid" $ codecmulti $ tomidi d
+                  x <- getChar
+                  case x of
+                       'q'    -> putStrLn ""
+                       '\DEL' -> case d of
+                                      [] -> loop []
+                                      _  -> loop $ init d
+                       _      -> loop $ d++[x]
 main         = loop ""
+test = tomidi "c1 c1 c1 e2 d2 end"

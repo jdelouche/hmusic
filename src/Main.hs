af64257073e40efab29a85110e4883c53a42b30c
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-24 21:55:12 +0100

    verticale par defaut, horizontal avec /

--- src/Main.hs
+++ src/Main.hs
@@ -7,35 +7,39 @@ import Codec.Midi
 import Data.Typeable
 midchannels::[[(Int,[Char])]] -> [[(Ticks, Message)]]
 midchannels = fmap (foldr (\x a -> (Midi.midi x)++a) [])
-parser :: String -> [[(Int,[Char])]]
-parser x = Table.table $ Sep.sep $ x
-tomidi :: String -> [[(Ticks, Message)]]
-tomidi x = midchannels $ parser $ x
+parser :: ([(Int, Int, String)] -> [[(Int,String)]]) -> String -> [[(Int,[Char])]]
+parser f x = f $ Sep.sep x
+tomidi :: ([(Int, Int, String)] -> [[(Int,String)]]) -> String -> [[(Ticks, Message)]]
+tomidi f x = midchannels $ parser f x
 codecmulti n = Midi { fileType = MultiTrack, timeDiv  = TicksPerBeat 24, Codec.Midi.tracks   = n }
-loop s d       = do 
+loop o s d       = do 
                   putStr d
-                  if s then exportFile "mymusic.mid" $ codecmulti $ tomidi d
-                       else exportFile "mymusic.mid" $ codecmulti $ tomidi []
+                  let f = if o then Table.tableh else Table.tablev
+                  if s then exportFile "mymusic.mid" $ codecmulti $ tomidi f d
+                       else exportFile "mymusic.mid" $ codecmulti $ tomidi f []
                   x <- getChar
                   case x of
+                       '|'       -> loop False s d
+                       '/'       -> loop True  s d
                        'q'       -> putStrLn ""
-                       'z'       -> loop False d
-                       'y'       -> loop True d
+                       'z'       -> loop o False d
+                       'y'       -> loop o True d
                        '\DEL'    -> case d of
-                                      [] -> loop True []
+                                      [] -> loop o True []
                                       _  -> do let c = init d
-                                               loop True c
-                       '\n'   -> do loop True (d++[x])
+                                               loop o True c
+                       '\n'   -> do loop o True (d++[x])
                        _      -> do let n = d++[x]
-                                    loop True n
-main         = do loop True ""
-test1 = do let x="a1 a2 a3\n"++
-                 "b1 b2 b3\n"
+                                    loop o True n
+main         = do loop False True ""
+test1 = do let x="a1 a2 a3 end\n"++
+                  "b1 b2 b3 end\n"
            print x
-           let y = parser x     
+           let y = parser Table.tableh x     
            print y
            let z = midchannels y   
            print z
+           exportFile "mymusic.mid" $ codecmulti z
 
 test2 = do let x= "c1  pause    pause\n"++
                   "c1  pause    pause\n"++
@@ -49,4 +53,4 @@ test2 = do let x= "c1  pause    pause\n"++
                   "d1  pause    pause\n"++
                   "c1_ e1_      f1_\n"++
                   "end end      end"
-           exportFile "test.mid" $ codecmulti $ tomidi x
+           exportFile "test.mid" $ codecmulti $ tomidi Table.tablev x
